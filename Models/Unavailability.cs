f9e87773cace87ed74cc375c8109d94424ed7500
Hotzilla <tuukka.lindroos@gmail.com>
2026-02-14 18:52:17 +0200

    Create Blazor Pilkki weekend planner app

---
+++ Models/Unavailability.cs
@@ -0,0 +1,13 @@
+namespace hotzerver.pilkki.calendar.Models;
+
+public class Unavailability
+{
+    public int Id { get; set; }
+    public int ParticipantId { get; set; }
+    public Participant Participant { get; set; } = default!;
+    public int Year { get; set; }
+    public TripSeason Season { get; set; }
+    public DateOnly WeekendStart { get; set; }
+    public string? Comment { get; set; }
+    public DateTime UpdatedAtUtc { get; set; } = DateTime.UtcNow;
+}
