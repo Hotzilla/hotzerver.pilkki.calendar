346ca9060fc7b09ccc1ba329aac66b9bcd31c60a
Hotzilla <tuukka.lindroos@gmail.com>
2026-02-14 19:21:52 +0200

    Fix .NET 10 build errors and verify successful compile

---
+++ Models/Participant.cs
@@ -0,0 +1,8 @@
+namespace hotzerver.pilkki.calendar.Models;
+
+public class Participant
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+}
