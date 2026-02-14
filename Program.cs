5d7f6ea099f73fa6a5093089e120e9261c98e1c1
Hotzilla <tuukka.lindroos@gmail.com>
2026-02-14 19:57:06 +0200

    Add availability toggle to switch NOT OK back to OK

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using hotzerver.pilkki.calendar.Data;
+using hotzerver.pilkki.calendar.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseStaticWebAssets();
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddDbContext<PilkkiDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<WeekendService>();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PilkkiDbContext>();
+    db.Database.EnsureCreated();
+    SeedData.EnsureSeeded(db);
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<hotzerver.pilkki.calendar.Components.App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
