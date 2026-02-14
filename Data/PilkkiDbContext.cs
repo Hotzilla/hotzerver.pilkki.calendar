643d54fa3085e9ae3011890157accaa01655b90c
Hotzilla <tuukka.lindroos@gmail.com>
2026-02-14 19:38:36 +0200

    Fix Blazor interactivity so name click opens modal

---
+++ Data/PilkkiDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using hotzerver.pilkki.calendar.Models;
+
+namespace hotzerver.pilkki.calendar.Data;
+
+public class PilkkiDbContext(DbContextOptions<PilkkiDbContext> options) : DbContext(options)
+{
+    public DbSet<Participant> Participants => Set<Participant>();
+    public DbSet<Unavailability> Unavailabilities => Set<Unavailability>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Participant>()
+            .HasIndex(p => new { p.FirstName, p.LastName })
+            .IsUnique();
+
+        modelBuilder.Entity<Unavailability>()
+            .HasIndex(u => new { u.ParticipantId, u.Year, u.Season, u.WeekendStart })
+            .IsUnique();
+
+        modelBuilder.Entity<Unavailability>()
+            .HasOne(u => u.Participant)
+            .WithMany()
+            .HasForeignKey(u => u.ParticipantId);
+    }
+}
